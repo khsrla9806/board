67b77daabe73cf61061191b3c9eefa39229db0cf
khsela9806 <kimhunsope@naver.com>
2024-02-16 16:57:13 +0900

    chore: 테스트 어노테이션 제거

--- src/test/java/com/board/boardproject/BoardProjectApplicationTests.java
+++ src/test/java/com/board/boardproject/BoardProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.board.boardproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BoardProjectApplicationTests {
 
 	@Test
