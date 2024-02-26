2eebb89c5eddb64b1387f3287af2c7dcaad25d36
khsela9806 <kimhunsope@naver.com>
2024-02-26 14:22:08 +0900

    feature: 게시글 수정, 삭제 Mapper 구현

--- src/main/java/com/board/boardproject/repository/BoardRepository.java
+++ src/main/java/com/board/boardproject/repository/BoardRepository.java
@@ -8,7 +8,29 @@ import com.board.boardproject.entity.Board;
 
 @Mapper
 public interface BoardRepository {
+	/**
+	 * 게시글 목록 조회
+	 * TODO: 페이징으로 변환
+	 */
 	List<Board> findAll();
+	
+	/**
+	 * 게시글 단건 조회
+	 */
 	Board findById(Long id);
+	
+	/**
+	 * 게시글 저장
+	 */
 	void save(Board board);
+	
+	/**
+	 * 게시글 수정
+	 */
+	void update(Board board);
+	
+	/**
+	 * 게시글 삭제
+	 */
+	void deleteById(Long id);
 }
