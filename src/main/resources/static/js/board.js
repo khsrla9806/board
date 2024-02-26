34f5f15e20f86dac02eace23f7fcae7304bda81a
khsela9806 <kimhunsope@naver.com>
2024-02-26 17:36:15 +0900

    feature: 페이지네이션 버튼 로직 추가 (비활성 정보는 아직 No)

--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -17,17 +17,32 @@ function getBoards() {
 }
 
 function getBoardsPage(page) {
+	
+	if (page < 0) {
+		alert("첫 번째 페이지입니다.");
+		return;
+	}
+	
+	if (page == undefined) {
+		page = 0;
+	}
 	$.ajax({
 		type: 'get',
-		url: `/api/v1/boards/page?page=${page == undefined ? 0 : page}`
+		url: `/api/v1/boards/page?page=${page}`
 	}).done(response => {
+		
+		if (page >= response.totalPages) {
+			alert("마지막 페이지입니다.");
+			return;
+		}
+		
 		$('#board-list').empty();
 		response.data.forEach((board) => {
 			let boardItem = getBoardItem(board);
 			$('#board-list').append(boardItem);
 		});
 		
-		setPagination(response);
+		setPagination(page, response);
 		
 		// 게시글 리스트 상단으로 스크롤 이동
 		window.scrollTo({top: 0, behavior: 'smooth'});
@@ -39,15 +54,20 @@ function getBoardsPage(page) {
 
 getBoardsPage();
 
-function setPagination(response) {
+function setPagination(page, response) {
+	
+	// pagination UI 구성을 위해 필요한 정보들
+	const maxSize = 5;
+	const startPage = Math.floor(page / maxSize) * maxSize + 1;
+	const lastPage = startPage + (maxSize - 1) > response.totalPages ? response.totalPages : startPage + (maxSize - 1);
 	
 	let pagination = `
-		<a class="arrow pprev" href="javascript:void(0)"></a>
-      	<a class="arrow prev" href="javascript:void(0)"></a>
+		<a class="arrow pprev" href="javascript:void(0)" onClick="getBoardsPage(${page - 1})"></a>
+      	<a class="arrow prev" href="javascript:void(0)" onClick="getBoardsPage(${page - 1})"></a>
 	`;
 	
 	
-	for (let i = 1; i <= response.totalPages; i++) {
+	for (let i = startPage; i <= lastPage; i++) {
 		
 		if (response.pageable.pageNumber + 1 == i) {
 			pagination += `
@@ -62,8 +82,8 @@ function setPagination(response) {
 	
 	
 	pagination += `
-		<a class="arrow next" href="javascript:void(0)"></a>
-		<a class="arrow nnext" href="javascript:void(0)"></a>
+		<a class="arrow next" href="javascript:void(0)" onClick="getBoardsPage(${page + 1})"></a>
+		<a class="arrow nnext" href="javascript:void(0)" onClick="getBoardsPage(${page + 1})"></a>
 	`;
 	
 	$('#page_nation').empty().append(pagination);
